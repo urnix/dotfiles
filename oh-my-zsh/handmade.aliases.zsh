0237441b32a1d30845b8f2e8125a475aadd67e54
Anton Shubin <antoxas12@gmail.com>
2015-11-12 21:03:55 +0300

    add oh-my-zsh

---
+++ oh-my-zsh/handmade.aliases.zsh
@@ -0,0 +1,22 @@
+# Helpers
+alias df="df -h"                            # Disk free, in gigabytes, not bytes
+alias server="python -m SimpleHTTPServer"
+alias o="open ."                        # Open folder in Finder
+
+# Applications
+alias st="open -a 'SublimeText2'"   # Open in SublimeText
+
+# NPM
+alias ni="npm i"                    
+alias nig="npm i -g"                
+alias nis="npm i --save"            
+alias nisd="npm i --save-dev"       
+alias nup="npm update"             
+alias nu="npm uninstall"                    
+
+# Bower
+alias bi="bower i"                
+alias bis="bower i --save"        
+alias bisd="bower i --save-dev"   
+alias bup="bower update"     
+alias bu="bower uninstall"           
