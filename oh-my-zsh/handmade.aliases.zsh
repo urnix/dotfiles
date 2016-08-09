8fd7fbea4e8eb88d81bb061e08aacb9b4e9c984b
Anton Shubin <antoxas12@gmail.com>
2016-08-09 22:40:33 +0200

    Make commands shorter

--- oh-my-zsh/handmade.aliases.zsh
+++ oh-my-zsh/handmade.aliases.zsh
@@ -1,22 +1,21 @@
 # Helpers
 alias df="df -h"                            # Disk free, in gigabytes, not bytes
 alias server="python -m SimpleHTTPServer"
-alias o="open ."                        # Open folder in Finder
+alias o="open"                        # Open folder in Finder
 
 # Applications
 alias st="open -a 'SublimeText2'"   # Open in SublimeText
 
 # NPM
-alias ni="npm i"                    
-alias nig="npm i -g"                
-alias nis="npm i --save"            
-alias nisd="npm i --save-dev"       
-alias nup="npm update"             
-alias nu="npm uninstall"                    
+alias nig="npm i -g"
+alias ni="npm i --save"
+alias nid="npm i --save-dev"
+alias nup="npm update"
+alias nu="npm uninstall --save"
+alias nud="npm uninstall --save-dev"
 
 # Bower
-alias bi="bower i"                
-alias bis="bower i --save"        
-alias bisd="bower i --save-dev"   
-alias bup="bower update"     
-alias bu="bower uninstall"           
+alias bi="bower i --save"
+alias bid="bower i --save-dev"
+alias bup="bower update"
+alias bu="bower uninstall --save"
