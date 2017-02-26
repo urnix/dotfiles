64b27ff52b17ebdf50a9440bdeb5f44ae78e16e5
Anton Shubin <2spy4x@gmail.com>
2017-02-26 13:40:41 +0300

    (.oh-my-zsh/custom/spy4x.aliases.zsh): remove npm and bower aliases, add yarn aliases

--- .oh-my-zsh/custom/spy4x.aliases.zsh
+++ .oh-my-zsh/custom/spy4x.aliases.zsh
@@ -6,16 +6,8 @@ alias o="open"                        # Open folder in Finder
 # Applications
 alias st="open -a 'SublimeText2'"   # Open in SublimeText
 
-# NPM
-alias nig="npm i -g"
-alias ni="npm i --save"
-alias nid="npm i --save-dev"
-alias nup="npm update"
-alias nu="npm uninstall --save"
-alias nud="npm uninstall --save-dev"
-
-# Bower
-alias bi="bower i --save"
-alias bid="bower i --save-dev"
-alias bup="bower update"
-alias bu="bower uninstall --save"
+# Yarn
+alias ya="yarn add"  # install package to dependencies
+alias yad="yarn add -D"  # install package to devDependencies
+alias yag="yarn global add"  # install package globally
+alias yu="yarn remove"
