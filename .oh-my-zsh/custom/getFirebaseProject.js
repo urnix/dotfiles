37d613f93295b2154988dd2357f83dde95a1542a
Anton Shubin <2spy4x@gmail.com>
2020-04-07 21:50:16 +0300

    fix(.oh-my-zsh/custom/getFirebaseProject.js): add error handling if "firebase-tools.json" was not found

--- .oh-my-zsh/custom/getFirebaseProject.js
+++ .oh-my-zsh/custom/getFirebaseProject.js
@@ -1,7 +1,19 @@
-const json = require(`${process.env.HOME}/.config/configstore/firebase-tools.json`);
+const { readFileSync } = require(`fs`);
 
-const firebaseProjectName = json.activeProjects[process.argv[2]];
+const folderPath = process.argv[2];
+try {
+    const pathToFirebaseToolsJson = `${process.env.HOME}/.config/configstore/firebase-tools.json`;
+    const fileContent = readFileSync(pathToFirebaseToolsJson);
+    const firebaseToolsJson = JSON.parse(fileContent);
+    const firebaseProjectName = firebaseToolsJson.activeProjects[folderPath];
 
-if (firebaseProjectName) {
-    console.log(firebaseProjectName);
+    if (firebaseProjectName) {
+        console.log(firebaseProjectName);
+    }
+} catch (error) {
+    if (error.code && error.code === "ENOENT") {
+        console.error(`Couldn't find file "${pathToFirebaseToolsJson}". Please create it with "$ firebase use <projectName>" in your project folder.`);
+    } else {
+        console.error(error);
+    }
 }
